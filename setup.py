9d6bdab9ca81440fde52134382384787af37c11b
Michael Atkinson <michael.atkinson@manchester.ac.uk>
2020-03-19 13:08:58 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='beta_reconstruction',
+    version='0.1',
+    packages=find_packages(exclude=["tests"]),
+    url='github.com/LightForm-group/beta-reconstruction',
+    license='MIT',
+    author='Michael D. Atkinson',
+    author_email='michael.atkinson@manchesterr.ac.uk',
+    install_requires=[
+        'numpy',
+        'defdap'
+    ]
+)
